4f337ba71f7fa914f7c993adf079b3922a4fb6b9
TannyKim <taekyun.contact@gmail.com>
2022-03-17 22:27:41 +0900

    DATABASES 추가

--- projects/mysite/config/settings/prod.py
+++ projects/mysite/config/settings/prod.py
@@ -4,3 +4,14 @@ ALLOWED_HOSTS = ['54.180.54.179', 'tandevtil.site']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
+
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'pybo',
+        'USER': 'dbmasteruser',
+        'PASSWORD': '8NIKr1xh#mWG3%wbQX-<7}.RJ!w{],$,',
+        'HOST': 'ls-fed61a92734f9c9a4c04b9d59c6b674e9571a55e.cygybgxia1m9.ap-northeast-2.rds.amazonaws.com',
+        'PORT': '5432',
+    }
+}
